4b44074cb0f525194161b015bd27ab0036f21aee
Prokopovych <olesservetnyk@gmail.com>
2021-07-13 10:52:32 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ReverseGeo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Program started. Press Enter to start coding");
+            Console.ReadKey();
+
+            #region Preparation
+            #endregion
+
+            #region Communicate with Google
+            #endregion
+
+            #region Show results
+            #endregion 
+
+            Console.WriteLine("Program ended. Press Enter to exit");
+            Console.ReadKey();
+        }
+    }
+}
